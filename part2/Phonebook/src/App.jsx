9ac83c4765ea43f97dfc644e9d3cf3cb149d8efb
muhammedma-2006 <muhammedmaponnani@gmail.com>
2026-02-11 11:16:29 +0530

    part2

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState  } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Personservices from './services/Personservices';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [notification, setNotification] = useState(null);
+  const [errormessage, setErrormessage] = useState(null);
  
+
+
+useEffect(() => {
+  Personservices
+  .getAll()
+  .then(response => {
+    setPersons(response.data);
+  })
+}, [])
+
  const handleNnameChange = (event) => {
        setNewName(event.target.value)  
 
@@ -21,12 +29,74 @@ const App = () => {
   
   const handlePerson = (event) => {
   event.preventDefault();
-    if(persons.some(person => person.name.toLowerCase() === newName.toLowerCase())){
-      alert(`${newName} is already added to phonebook`)
+
+  if(persons
+      .some(person => 
+        person.name.toLowerCase() === newName.toLowerCase())){
+
+    if (window.confirm(`${newName} is already added to phonebook, Do you want to update the number?`)) {
+    const person = persons.find(p => p.name.toLowerCase() === newName.toLowerCase());
+    const updatedPerson = { ...person, number: newNumber };
+    Personservices
+      .update(person.id, updatedPerson)
+      .then(response => {
+        setPersons(persons.map(p => p.id !== person.id ? p : response.data));
+        setNewName('');
+        setNewNumber('');
+      
+      })
+      .catch(error => {
+        setErrormessage(`Information of ${newName} has already been removed from server`);
+        setTimeout(() => {
+          setErrormessage(null);
+        }, 5000);
+      });
     }
-      setPersons([...persons, {name: newName, number: newNumber, id: persons.length + 1}])
-   setNewName('');
-   setNewNumber('');
+  }
+  else {
+  Personservices.create({ name: newName,
+                          number: newNumber})
+    .then(response => {
+      setPersons(persons.concat(response.data));
+      setNotification(`Added ${newName}`);
+      setTimeout(() => {
+        setNotification(null);
+      }, 5000);
+      setNewName('');
+      setNewNumber('');
+    })
+    .catch(error => {
+      setErrormessage(error.response.data.error);
+      setTimeout(() => {
+        setErrormessage(null);
+      }, 5000);
+    });
+  }
+  }
+  
+
+  const handleDelete = (id) => {
+    const person = persons.find(p => p.id === id);
+    if (person && window.confirm(`Delete ${person.name}?`)) {
+      Personservices
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id));
+        })
+        .then(
+          setNotification(`Deleted ${person.name}`),
+          setTimeout(() => {
+            setNotification(null);
+          }, 5000)
+        )
+        .catch(error => {
+          setErrormessage(`Information of ${person.name} has already been removed from server`);
+          setTimeout(() => {
+            setErrormessage(null);
+          }, 5000);
+        }
+        );
+    }
   }
   const filteredPersons = 
     persons.filter(person =>
@@ -48,7 +118,8 @@ const App = () => {
               handleNumberChange={(e) => setNewNumber(e.target.value)}
       />
       <h2>Numbers</h2>
-      <Persons personsToShow={filteredPersons} />
+      <Persons  personsToShow={filteredPersons} 
+                handleDelete={handleDelete} />
     </div>
   )
 }
--- part2/Phonebook/src/components/Persons.jsx
+++ part2/Phonebook/src/components/Persons.jsx
@@ -1,7 +1,15 @@
-const Persons = ({ personsToShow }) => {
+const Persons = ({ personsToShow, handleDelete }) => {
   return (
     <ul>
-      {personsToShow.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
+      {personsToShow
+        .map(person => 
+              <li   key={person.id}>
+                    {person.name}&nbsp;
+                    {person.number} 
+                    <button onClick={() => handleDelete(person.id)}>
+                      delete
+                    </button>
+              </li>)}
     </ul>
   )
 }
